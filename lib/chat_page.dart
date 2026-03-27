0bf1e0690788726c319fab4f8f53334406c48809
Firdaus <firdausfaisal2002@gmail.com>
2026-03-27 10:32:41 +0800

    update: chat input would not scroll as well and also utilize Flexible widget

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -15,104 +15,110 @@ class ChatPage extends StatelessWidget {
         ],
       ),
 
-      body: ListView(
-        children:[ 
-          Column(
-            children: [
-              Align(
-                alignment: AlignmentGeometry.centerLeft,
-                child: Container(              
-                  margin: EdgeInsets.all(50),
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                      bottomRight: Radius.circular(20)
+      body: Column(
+        children: [
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 1,
+            child: ListView(
+              children:[ 
+                Align(
+                  alignment: AlignmentGeometry.centerLeft,
+                  child: Container(              
+                    margin: EdgeInsets.all(50),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20)
+                      ),
                     ),
-                  ),
-                
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text("This is your message", style: TextStyle(color: Colors.white),),
-                      SizedBox(height: 20,),
-                      Image.network('https://media.tenor.com/T1zotsnaPJsAAAAe/bruh-meme.png', scale: 5,)
-                    ],
-                  ),
-                
-                ),
-              ),
-              
-              Align(
-                alignment: AlignmentGeometry.centerRight,
-                child: Container(             
-                  margin: EdgeInsets.all(50),
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                      bottomRight: Radius.circular(20)
+                  
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("This is your message", style: TextStyle(color: Colors.white),),
+                        SizedBox(height: 20,),
+                        Image.network('https://media.tenor.com/T1zotsnaPJsAAAAe/bruh-meme.png', scale: 5,)
+                      ],
                     ),
-                  ),
-                
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text("This is your message", style: TextStyle(color: Colors.white),),
-                      SizedBox(height: 20,),
-                      Image.network('https://media.tenor.com/T1zotsnaPJsAAAAe/bruh-meme.png', scale: 5,)
-                    ],
-                  ),
-                
-                ),
-              ),
-          
-              Container(
-                
-                margin: EdgeInsets.all(50),
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                    bottomRight: Radius.circular(20)
+                  
                   ),
                 ),
-              
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text("This is your message", style: TextStyle(color: Colors.white),),
-                    SizedBox(height: 20,),
-                    Image.network('https://media.tenor.com/T1zotsnaPJsAAAAe/bruh-meme.png', scale: 5,)
-                  ],
+                
+                Align(
+                  alignment: AlignmentGeometry.centerRight,
+                  child: Container(             
+                    margin: EdgeInsets.all(50),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20)
+                      ),
+                    ),
+                  
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("This is your message", style: TextStyle(color: Colors.white),),
+                        SizedBox(height: 20,),
+                        Image.network('https://media.tenor.com/T1zotsnaPJsAAAAe/bruh-meme.png', scale: 5,)
+                      ],
+                    ),
+                  
+                  ),
                 ),
-              
-              ),
-
-              Container(
-                height: 80,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(20))
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(onPressed: (){}, icon: Icon(Icons.add, color: Colors.white,)),
-                    IconButton(onPressed: (){}, icon: Icon(Icons.send, color: Colors.white,))
-
-                  ],
-                ),
-              )
-            ],
+                              
+                Align(
+                  alignment: AlignmentGeometry.centerLeft,
+                  child: Container(              
+                    margin: EdgeInsets.all(50),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20)
+                      ),
+                    ),
+                  
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("This is your message", style: TextStyle(color: Colors.white),),
+                        SizedBox(height: 20,),
+                        Image.network('https://media.tenor.com/T1zotsnaPJsAAAAe/bruh-meme.png', scale: 5,)
+                      ],
+                    ),
+                  
+                  ),
+                ),              
+              ]
+            ),
           ),
-        ]
+
+          Container(
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20))
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.add, color: Colors.white,)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.send, color: Colors.white,))
+                    
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
